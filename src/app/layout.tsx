616ab68eac76698ee766808e898467f48bee60dd
Filipe Bacof <filipebacof@gmail.com>
2023-06-06 01:19:21 -0300

    second class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import { ReactNode } from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto_Flex as Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
 export const metadata = {
   title: 'NLW Spacetime',
   description:
-    'Projeto da trilha explorer efetuado no evento Next Level Week da Rocketseat',
+    'Uma cápsula do tempo efetuado no evento Next Level Week da Rocketseat',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} bg-gray-900 text-gray-50`}>
+        {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,44 @@ import BebendoComRafa from '../../public/25-03-2022.jpg'
 
 export default function Home() {
   return (
-    <main className="">
-      <div>
-        <small>19 de Agosto de 2022</small>
-        <h1>Anubis no seu primeiro dia conosco</h1>
-        <Image src={DieliComGato} alt="Dieli segurando o Anubis" />
-        <p>
+    <nav className="p-16">
+      <a
+        className="flex flex-col gap-4 no-underline"
+        href="https://portifolio-filipe-bacof.vercel.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <small className="text-xs text-gray-100">19 de Agosto de 2022</small>
+        <h1 className="text-2xl text-gray-50">
+          Anubis no seu primeiro dia conosco
+        </h1>
+        <Image
+          className="h-[280px] w-full rounded-lg object-cover"
+          src={DieliComGato}
+          alt="Dieli segurando o Anubis"
+        />
+        <p className="text-lg text-gray-100">
           Neste dia, Dieli e eu fomos adotar nosso gato. Ele estava extremamente
           assustado inicialmente, mas não demorou muito para que ele começasse a
           explorar a casa e encontrar seu lugar favorito: a janela, de onde ele
           podia observar os pássaros e os gatos dos vizinhos.
         </p>
-      </div>
+      </a>
 
-      <div>
-        <small>25 de Março de 2022</small>
-        <h1>Bacof e Mahl no Mendes Pub</h1>
-        <Image src={BebendoComRafa} alt="Eu e o Rafael brindando um chopp" />
-        <p>
+      <a
+        className="flex flex-col gap-4 no-underline"
+        href="https://portifolio-filipe-bacof.vercel.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <small className="text-xs text-gray-100">25 de Março de 2022</small>
+        <h1 className="text-2xl text-gray-50">Bacof e Mahl no Mendes Pub</h1>
+        <Image
+          className="h-[280px] w-full rounded-lg object-cover"
+          src={BebendoComRafa}
+          alt="Eu e o Rafael brindando um chopp"
+        />
+        <p className="text-lg text-gray-100">
           Foi um imenso prazer desenvolver uma amizade com essa pessoa especial.
           As risadas e as conversas que compartilhamos foram inesquecíveis. Além
           de ser um ótimo amigo, também é um programador excepcional.
@@ -29,7 +49,7 @@ export default function Home() {
             Espero beber contigo no bar que há lá do outro lado.
           </a>
         </p>
-      </div>
-    </main>
+      </a>
+    </nav>
   )
 }
